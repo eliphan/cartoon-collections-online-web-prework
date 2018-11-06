1b7d192de9385df1335f57419e3bfc3e2ce55bc2
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-06 23:46:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| 
+  planeteer_calls.any? { |call| call.length
 end
 
 def find_the_cheese# code an argument here
